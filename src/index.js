87a780eafbdadb1d724b3155214703bf57583f0d
Malte Schwerin <schwerin.malte@land-jungs.de>
2021-10-13 19:22:02 +0200

    fancy digital text animation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import "./css/index.scss";
 import "./js/main";
+
+var fadeElements = document.getElementsByClassName("fade");
+
+
+function fadeOutOnScroll(element, offset) {
+    if (!element) {
+        return;
+    }
+
+    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+    var elementHeight = element.offsetHeight;
+    var scrollTop = document.getElementById('scrollable').scrollTop;
+
+    var opacity = window.getComputedStyle(element).getPropertyValue("opacity");
+
+    console.log(element.opacity)
+    if (scrollTop > (distanceToTop - offset) && opacity == 1) {
+        element.style.opacity = 0;
+        console.log("make " + element.id + " invisible");
+    }
+
+    if (scrollTop <= (distanceToTop - offset) && opacity == 0) {
+        element.style.opacity = 1;
+        console.log("make " + element.id + " visible");
+    }
+}
+
+function scrollHandler() {
+    for (var i = 0; i < fadeElements.length; i++) {
+        fadeOutOnScroll(fadeElements[i], 540);
+    }
+}
+
+window.addEventListener('scroll', scrollHandler);
